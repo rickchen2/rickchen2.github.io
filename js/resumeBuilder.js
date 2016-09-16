c738dcbaec44be08e9f74395206feb200ea254de
rickchen2 <wallst.chen@gmail.com>
2016-09-15 23:36:29 -0400

    upload files
    upload files

---
+++ js/resumeBuilder.js
@@ -0,0 +1,6 @@
+var name = "Hongyu Chen";
+var role = "QA Analyst";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
