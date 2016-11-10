ae0ca0ea0be8b996aa2bc8ad705615d263dc5c2a
rickchen2 <wallst.chen@gmail.com>
2016-11-09 19:44:15 -0500

    Update from select button to drop down list

--- werewolves/js/colorChanger.js
+++ werewolves/js/colorChanger.js
@@ -1,101 +1,52 @@
 
-var myCounter = { killcounter:0, guardcounter:0, magiccounter:0};
+function OnSelectedIndexChange(id, selectid){
+
+	if (document.getElementById(selectid).value == "1" ){
+    	kill(id);
+    }
+    else if (document.getElementById(selectid).value == "2"){
+    	guard(id);
+    }
+    else if (document.getElementById(selectid).value == "3"){
+    	magic(id);
+    }
+    else if (document.getElementById(selectid).value == "4"){
+    	guardAndMagic(id);
+    }
+    else{
+    	reset(id);
+    }
+
+
+}
 
 
 function kill(id) {
 	var ccolor = document.getElementById(id);
-
-	if (myCounter.killcounter == 0 &&  myCounter.guardcounter == 0 && myCounter.magiccounter == 0) {
-		ccolor.style.backgroundColor = 'red';
-		myCounter.killcounter = 1;
-
-	}
-	
-	else if (myCounter.killcounter == 0 &&  myCounter.guardcounter == 1 && myCounter.magiccounter == 0){
-		ccolor.style.backgroundColor = '#42f492';
-		myCounter.killcounter = 1;
-
-	}
-	else if (myCounter.killcounter == 0 &&  myCounter.guardcounter == 0 && myCounter.magiccounter == 1){
-		ccolor.style.backgroundColor = '#42f492';
-		myCounter.killcounter = 1;
-
-	}
-	else if (myCounter.killcounter == 0 &&  myCounter.guardcounter == 1 && myCounter.magiccounter == 1){
-		ccolor.style.backgroundColor = 'red';
-		myCounter.killcounter = 1;
-
-	}
-	else	{
-		ccolor.style.backgroundColor = 'white';
-		myCounter.killcounter = 0;
-		myCounter.guardcounter =0;
-		myCounter.magiccounter =0;
-
-	}
+	ccolor.style.backgroundColor = 'red';
 }
-
-
 
 function guard(id) {
 	var ccolor = document.getElementById(id);
-
-	if ( myCounter.guardcounter == 0 && myCounter.killcounter == 0 && myCounter.magiccounter == 0) {
-		ccolor.style.backgroundColor = '#42f492';
-		myCounter.guardcounter = 1;
-
-	}
-	else if (myCounter.guardcounter == 0 && myCounter.killcounter == 0 && myCounter.magiccounter == 1) {
-		ccolor.style.backgroundColor = 'red';
-		myCounter.guardcounter = 1;
-
-	}
-	else if (myCounter.guardcounter == 0 && myCounter.killcounter == 1 && myCounter.magiccounter == 0) {
-		ccolor.style.backgroundColor = '#42f492';
-		myCounter.guardcounter = 1;
-
-	}
-	else if (myCounter.guardcounter == 0 && myCounter.killcounter == 1 && myCounter.magiccounter == 1) {
-		ccolor.style.backgroundColor = 'red';
-		myCounter.guardcounter = 1;
-	}
-	else {
-		ccolor.style.backgroundColor = 'white';
-		myCounter.guardcounter = 0;
-		myCounter.killcounter =0;
-		myCounter.magiccounter =0;
-	}
+	ccolor.style.backgroundColor = '#42f492';
 
 }
 
 function magic(id) {
 	var ccolor = document.getElementById(id);
-
-	if (myCounter.magiccounter == 0 && myCounter.killcounter == 0  &&  myCounter.guardcounter == 0 ) {
-		ccolor.style.backgroundColor = '#42f492';
-		myCounter.magiccounter = 1;
-
-	}
-	else if (myCounter.magiccounter == 0 && myCounter.killcounter == 1  &&  myCounter.guardcounter == 0) {
-		ccolor.style.backgroundColor = '#42f492';
-		myCounter.magiccounter = 1;
-
-	}
-	else if (myCounter.magiccounter == 0 && myCounter.killcounter == 0  &&  myCounter.guardcounter == 1) {
-		ccolor.style.backgroundColor = 'red';
-		myCounter.magiccounter = 1;
-
-	}
-	else if (myCounter.magiccounter == 0 && myCounter.killcounter == 1  &&  myCounter.guardcounter == 1) {
-		ccolor.style.backgroundColor = 'red';
-		myCounter.magiccounter = 1;
-
-	}
-	else {
-		ccolor.style.backgroundColor = 'white';
-		myCounter.magiccounter = 0;
-		myCounter.killcounter = 0;
-		myCounter.guardcounter =0;
-	}
+	ccolor.style.backgroundColor = '#42f492';
 
 }
+
+function guardAndMagic(id){
+	var ccolor = document.getElementById(id);
+	ccolor.style.backgroundColor = 'red';
+}
+
+function reset(id) {
+	var ccolor = document.getElementById(id);
+	ccolor.style.backgroundColor = 'white';
+
+}
+
+
